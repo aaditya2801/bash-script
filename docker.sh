e633ea9bb9b003d98508f542c974d2de31e2a352
aaditya2801 <66722117+aaditya2801@users.noreply.github.com>
2020-11-05 18:22:16 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 docker exec -it webserver yum install httpd git -y
 docker exec -it webserver git clone https://github.com/aaditya2801/menu.git
 docker exec -it webserver mv menu/index.html /var/www/html/
-docker exec -it webserver httpd
+docker exec -it webserver /usr/sbin/httpd ; httpd
 
